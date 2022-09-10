1cce53f7c449932468e74af5c19934d3bfa81ef0
Bonface Masira <bonfacemasira@gmail.com>
2022-09-10 22:58:29 +0300

    enable routing using NavLink and the outlet component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,40 +1,31 @@
 import React from "react";
-import Login from "../components/Login";
-import SignUp from "../components/SignUP";
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
+import {
+  NavLink,
+  Outlet,
+} from "react-router-dom";
 
 function Landingpage() {
-    return (
-        <div className="Landingpage">
-            <Router>
-                <div>
-                    <nav>
-                        <div>
-                            <ul>
-                                <li>
-                                    <NavLink to={'/login'}>
-                                        <button className="landingButton">Login</button>
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to={'/sign-up'}>
-                                        <button className="landingButton">Sign up</button>
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <div>
-                        <Routes>
-                            <Route exact path='/login' element={<Login />} />
-                            <Route exact path='/sign-up' element={<SignUp />} />
-                            <Route exact path='/' element={<Login />} />
-                        </Routes>
-                    </div>
-                </div>
-            </Router>
+  return (
+    <div className="Landingpage">
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to={"login"}>
+                        <button className="landingButton">Login</button>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"sign-up"}>
+                        <button className="landingButton">Sign up</button>
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+        <div>
+            <Outlet />
         </div>
-    )
+    </div>
+  );
 }
 
 export default Landingpage;
